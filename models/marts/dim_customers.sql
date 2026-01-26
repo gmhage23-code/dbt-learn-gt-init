65912fee148131c0c99726d0fd583601e103de47
austin rose <austin.rose@dbtlabsworkshop.com>
2026-01-26 15:54:24 +0000

    day2

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,7 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
@@ -12,8 +12,8 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
